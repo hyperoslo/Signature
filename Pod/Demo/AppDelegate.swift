465878d80164f8e1a10fe5c5cdee877cdc363a83
Elvis Nuñez <elvisnunez@me.com>
2015-04-19 16:13:41 +0200

    Clean up App Delegate

--- Pod/Demo/AppDelegate.swift
+++ Pod/Demo/AppDelegate.swift
@@ -5,13 +5,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    func application(
+        application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-
-        if let window = self.window {
-            window.rootViewController = ViewController()
-            window.makeKeyAndVisible()
-        }
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
 
         return true
     }
